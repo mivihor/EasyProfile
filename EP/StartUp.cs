0836e57500b84a6a117e6873a3c22ac7592825b9
SH@RK <igor.mykula@gmail.com>
2019-02-24 20:23:32 +0200

    User Control added

---
+++ EP/StartUp.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Autodesk.AutoCAD.Runtime;
+using Autodesk.AutoCAD.DatabaseServices;
+using Autodesk.AutoCAD.ApplicationServices;
+
+[assembly: CommandClass(typeof(EP.StartUp))]
+
+namespace EP
+{
+    class StartUp
+    {
+        [CommandMethod("EasyP")]
+
+        public void EasyP()
+        {
+            UControl form = new UControl();
+            Application.ShowModalWindow(form);
+        }
+    }
+}
---
+++ EP/UControl.xaml.cs
@@ -0,0 +1,44 @@
+﻿using Microsoft.Win32;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace EP
+{
+    /// <summary>
+    /// Interaction logic for UControl.xaml
+    /// </summary>
+    public partial class UControl : Window
+    {
+        public UControl()
+        {
+            InitializeComponent();
+        }
+
+        private void BrowseButton_Click(object sender, RoutedEventArgs e)
+        {
+            OpenFileDialog dlg = new OpenFileDialog();
+
+            dlg.DefaultExt = ".txt";
+            dlg.DefaultExt = ".xlsx";
+            dlg.Filter = "(*.txt)|*.txt|*.xlsx|*.xlsx|All Files (*.*)|*.*";
+
+            if (dlg.ShowDialog() == true)
+            {
+                string filename = dlg.FileName;
+                FilePathTextBox.Text = filename;
+            }
+        }
+    }
+}
