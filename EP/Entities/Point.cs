a0cc749069e5b0c6c50cf799ca17f9eb1c3feb12
mivihor <igor.mykula@gmail.com>
2019-03-16 13:13:43 +0200

    Entities/Point.cs added; Utility.cs added

---
+++ EP/Entities/Point.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EP.Entities
+{
+    public class Point
+    {
+        public double N { get; set; }
+        public double E { get; set; }
+        public double H { get; set; }
+    }
+}
